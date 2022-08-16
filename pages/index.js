96eb9a71587a158f8b7a3febd14c093f242305df
jiwonan <jwan0131@gmail.com>
2022-08-16 17:12:29 +0900

    Static Pre Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import { useState } from "react"
+
 export default function home() {
-    return <div><h1>Hello!</h1></div>
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <div>
+            <h1>
+                Hello! {counter}<br/>
+                <button onClick={() => {setCounter(prev => prev+1)}}>+</button>
+            </h1>
+        </div>
+    );
 }
